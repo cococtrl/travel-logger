f2d03c53189e5dec3f602daab61f388b6dd87c79
Courtney Coleman <uooa.coco@gmail.com>
2020-05-08 10:31:44 -0400

    added activity model

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.urls import reverse
 from datetime import date
 
+TICKETS = (
+    ('Y', 'Purchased'),
+    ('N', 'Not Purchased'),
+    ('NA', 'Not Needed'),
+)
+
 class Trip(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=250)
@@ -13,3 +19,14 @@ class Trip(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'trip_id': self.id})
+
+class Activity(models.Model):
+    date = models.DateField()
+    activity = models.CharField(max_length=50)
+    tickets = models.CharField(
+        max_length=2,
+        choices=TICKETS,
+        default=TICKETS[0][0])
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.get_tickets_display()} on {self.date}"
