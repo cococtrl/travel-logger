cbe771d5dba2681f0b8197ec36eaf144fb93fc6f
Courtney Coleman <uooa.coco@gmail.com>
2020-05-13 09:24:58 -0400

    photo registered to admin site

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Trip, Activity, Landmark
+from .models import Trip, Activity, Landmark, Photo
 # Register your models here.
 
 admin.site.register(Trip)
 admin.site.register(Activity)
 admin.site.register(Landmark)
+admin.site.register(Photo)
